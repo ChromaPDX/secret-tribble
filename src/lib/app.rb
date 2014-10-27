bcf091760a435b14884bb86841062e4f280d193c
Peat Bakke <peat@peat.org>
2014-10-27 15:51:41 -0700

    Using system wide configs; sequel to connect

--- src/lib/app.rb
+++ src/lib/app.rb
@@ -1,23 +1,44 @@
+require 'sequel'
 require 'pg'
+require 'json'
 
 module App
-
-  @@db_config = {
-    host: ENV['CHROMA_DB_HOST'] || 'localhost',
-    port: ENV['CHROMA_DB_PORT'] || '5432',
-    dbname: ENV['CHROMA_DB_NAME'] || 'chroma_dev',
-    user: ENV['CHROMA_DB_USER'] || 'vagrant',
-    password: ENV['CHROMA_DB_PASSWORD'] || 'vagrant',
-  }.delete_if { |k,v| v.nil? || v.empty? }
-
-  @@db_connection = false
   
-  def self.db_connection
-    if @@db_connection and @@db_connection.status == "CONNECTION_OK"
-      @@db_connection
-    else
-      @@db_connection = PG::Connection.open(@@db_config)
-    end
+  @@config_path = false
+  @@config = {}
+  
+  def self.configure!( env_name )
+
+    # load system wide configuration files
+    @@config_path = File.join( File.dirname(__FILE__), "..", "config", "#{env_name}.json" )
+    @@config = JSON.parse( IO.read(@@config_path) )
+
+    # pull in configuration information from the shell environment.
+    default_db_config = {
+      adapter: 'postgres',
+      host: ENV['CHROMA_DB_HOST'],
+      port: ENV['CHROMA_DB_PORT'],
+      database: ENV['CHROMA_DB_NAME'],
+      user: ENV['CHROMA_DB_USER'],
+      password: ENV['CHROMA_DB_PASSWORD'],
+    }.delete_if { |k,v| v.nil? or v.empty? }
+
+    # overwrite default_config with the "db" section of @@config
+    db_config = default_db_config.merge( @@config["db"] )
+
+    # connect, yo
+    @@db = Sequel.connect( db_config )
+
+    # if we got this far ...
+    true
+  end
+  
+  def self.config
+    @@config
+  end
+  
+  def self.db
+    @@db
   end
   
 end
