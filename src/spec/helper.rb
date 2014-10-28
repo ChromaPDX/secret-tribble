ad31f2eb31daeee5e3bf8f7c2add71700debd75e
Peat Bakke <peat@peat.org>
2014-10-28 11:25:29 -0700

    Cleaning database after all tests run

--- src/spec/helper.rb
+++ src/spec/helper.rb
@@ -4,4 +4,11 @@ RSpec.configure do |cfg|
   cfg.before(:suite) do
     App.configure! "test"
   end
+
+  cfg.after(:suite) do
+    # wipe everything out of the test database
+    [:distributions].each do |table|
+      App.db[table].delete
+    end
+  end
 end
