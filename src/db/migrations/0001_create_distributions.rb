0ebf4740f4e73cde164ebbca21bea0d66ca7810c
Peat Bakke <peat@peat.org>
2014-10-28 11:13:44 -0700

    Fixing indexes, hard fail if the table already exists.

--- src/db/migrations/0001_create_distributions.rb
+++ src/db/migrations/0001_create_distributions.rb
@@ -1,16 +1,18 @@
 Sequel.migration do
   up do
     App.db.run <<-eos
-      CREATE TABLE IF NOT EXISTS distributions (
+      CREATE TABLE distributions (
         distribution_id VARCHAR(32) PRIMARY KEY,
         pool_id VARCHAR(32) NOT NULL,
         account_id VARCHAR(32) NOT NULL,
-        split_pct NUMERIC(5, 1) NOT NULL,
+        split_pct DECIMAL(5,4) NOT NULL,
         created_at TIMESTAMP NOT NULL
       )
     eos
-    add_index created_at
-    add_index pool_id
+    
+    add_index :distributions, :created_at
+    add_index :distributions, :pool_id
+    add_index :distributions, :account_id
   end
 
   down do
