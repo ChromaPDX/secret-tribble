84ad0d55b31e4dd1a0a51fa7d3ad4ad8f880cc15
Peat Bakke <peat@peat.org>
2014-10-28 16:49:58 -0700

    Cleans up log files before running tests

--- src/spec/helper.rb
+++ src/spec/helper.rb
@@ -1,8 +1,15 @@
 require_relative '../lib/app'
+require 'fileutils'
 
 RSpec.configure do |cfg|
   cfg.before(:suite) do
-    App.configure! (ENV['CHROMA_ENV'] || "test")
+    chroma_env = (ENV['CHROMA_ENV'] || 'test')
+    
+    # clean out the log files for the environment
+    FileUtils.rm_f App.log_file_path(chroma_env)
+
+    # set up the environment
+    App.configure! chroma_env
   end
 
   cfg.after(:suite) do
