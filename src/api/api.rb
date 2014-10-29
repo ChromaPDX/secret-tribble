3b66fdbda2a4e1d77b1a0b3ba5bd69db845e2544
Peat Bakke <peat@peat.org>
2014-10-29 11:30:29 -0700

    ENV fix for running on vagrant; SOON horse landing page

--- src/api/api.rb
+++ src/api/api.rb
@@ -2,10 +2,10 @@ require 'sinatra'
 require_relative '../lib/app.rb'
 require_relative '../lib/distribution.rb'
 
-App.configure! ENV['CHROMA_ENV']
+App.configure!( ENV['CHROMA_ENV'] || 'vagrant' )
 
 get '/' do
-  "hello, world! #{App.db[:distributions].count}"
+  '<img style="width: 100%; height: 100%" src="http://img4.wikia.nocookie.net/__cb20130627171445/safari-zone/images/c/c1/Soon-horse.jpg">'
 end
 
 get '/v1/distributions' do
