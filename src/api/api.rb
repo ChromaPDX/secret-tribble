ce90964b879a1a1647654e1b7ed760cb0a7144bc
Peat Bakke <peat@peat.org>
2014-10-28 21:28:31 -0700

    Hey look, a distribution end point

--- src/api/api.rb
+++ src/api/api.rb
@@ -1,8 +1,17 @@
 require 'sinatra'
 require_relative '../lib/app.rb'
+require_relative '../lib/distribution.rb'
 
 App.configure!
 
 get '/' do
   "hello, world! #{App.db[:distributions].count}"
+end
+
+get '/v1/distributions' do
+  pool_id = params[:pool_id]
+  d = Distribution.new( pool_id )
+  d.load!
+
+  d.splits.to_json
 end
