f96f345bde75bb379c2418eedafc4f456f9b2af7
Peat Bakke <peat@peat.org>
2014-11-03 10:47:34 -0800

    Consolidating common helper methods

--- src/api/spec/helper.rb
+++ src/api/spec/helper.rb
@@ -10,6 +10,9 @@ App.configure!( ENV['CHROMA_ENV'] )
 
 require_relative '../api.rb'
 
+require_relative '../../lib/distribution.rb'
+require_relative '../../lib/token.rb'
+
 RSpec.configure do |cfg|
   cfg.before(:suite) do
     # wipe everything out of the test database
@@ -17,4 +20,22 @@ RSpec.configure do |cfg|
       App.db[table].delete
     end
   end
+end
+
+# HELPER METHODS -------------------------------------------------------------
+
+# We have several custom headers to assist with debugging
+def check_headers( resp )
+  expect( resp.headers['Content-Type'] ).to eq("application/json")
+  expect( resp.headers['Chroma-Processing-MS'] ).to_not be_nil
+end
+
+
+def valid_distribution
+  d = Distribution.new( App.unique_id )
+  d.split!("alice", BigDecimal.new("0.5"))
+  d.split!("bob", BigDecimal.new("0.3"))
+  d.split!("carol", BigDecimal.new("0.2"))
+
+  d
 end
