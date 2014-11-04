98015ea6a7ddf9acd53e8d7779a8383ed719d380
Peat Bakke <peat@peat.org>
2014-11-03 16:58:01 -0800

    Phew, secured token generation and checking endpoint! woo!

--- src/api/spec/tokens_spec.rb
+++ src/api/spec/tokens_spec.rb
@@ -9,22 +9,65 @@ describe '/v1/tokens' do
   end
   
   before(:all) do
-    @account_id = App.unique_id
+    @secret_key = App.unique_id
+    @account = Account.create!("test account")
+    @account_id = @account.account_id
+    @account.set_secret_key( @secret_key )
+
     @metadata = "example_token"
     @token = Token.create!( @account_id, @metadata )
   end
+  
+  
+  it "POST given correct credentials, it should create a new token" do
+    # success case
+    post "/v1/tokens.json", account_id: @account_id, secret_key: @secret_key
+    expect(last_response).to be_ok
+    j = JSON.parse(last_response.body)
+    expect(j["account_id"]).to eq(@account_id)
+    expect(j["token_id"]).to_not be nil
+
+    # failure case: bad account_id
+    post "/v1/tokens.json", account_id: "asfasf", secret_key: @secret_key
+    expect(last_response.status).to be 401 # Not Authenticated
+    j = JSON.parse(last_response.body)
+    expect(j['errors']).to be_an(Array)
+    
+    # failure case: missing account_id and secret_key
+    post "/v1/tokens.json"
+    expect(last_response.status).to be 401 # Not Authenticated
+    j = JSON.parse(last_response.body)
+    expect(j['errors']).to be_an(Array)
+  end
 
   
-  it "POST /v1/tokens.json given correct credentials, it should create a new token"
+  it "GET given correct credentials, it should list all tokens" do
+    # success case
+    get "/v1/tokens.json", account_id: @account_id, secret_key: @secret_key
+    expect(last_response).to be_ok
+    j = JSON.parse(last_response.body)
+    expect(j["tokens"]).to be_an(Array)
+    j["tokens"].each do |t|
+      expect(t["account_id"]).to eq(@account_id)
+    end
+
+    # failure case: bad account_id
+    get "/v1/tokens.json", account_id: "asfasf", secret_key: @secret_key
+    expect(last_response.status).to eq(401) # Not Authenticated
+    j = JSON.parse(last_response.body)
+    expect(j['errors']).to be_an(Array)
+    
+    # failure case: missing account_id and secret_key
+    get "/v1/tokens.json"
+    expect(last_response.status).to eq(401) # Not Authenticated
+    j = JSON.parse(last_response.body)
+    expect(j['errors']).to be_an(Array)
+  end
 
   
-  it "GET /v1/tokens.json given correct credentials, it should list all tokens"
-
+  it "DELETE given correct credentials, should delete a token"
   
-  it "DELETE /v1/tokens.json given correct credentials, should delete a token"
-  
-  
-  it "GET /v1/tokens.json should verify that a token is active" do
+  it "GET should verify that a token is active" do
     # success
     get "/v1/tokens.json", token_id: @token.token_id
     expect(last_response).to be_ok
@@ -32,13 +75,13 @@ describe '/v1/tokens' do
 
     # failure
     get "/v1/tokens.json", token_id: App.unique_id
-    expect(last_response.status).to eq(404)
+    expect(last_response.status).to eq(401)
     j = JSON.parse(last_response.body)
     expect( j['errors'].empty? ).to be false
   end
 
   
-  it "GET /v1/tokens.json should verify that all of the token data is available" do
+  it "GET should verify that all of the token data is available" do
     get "/v1/tokens.json", token_id: @token.token_id
     expect(last_response).to be_ok
     check_headers(last_response)
--- src/api/v1/tokens.rb
+++ src/api/v1/tokens.rb
@@ -1,18 +1,79 @@
 require_relative '../../lib/token'
 
+def token_present?
+  !params[:token_id].nil?
+end
+
+
+def secret_auth_present?
+  params[:account_id] and params[:secret_key]
+end
+
+
+def require_secret_key!
+  return false unless secret_auth_present?
+  
+  @account = Account.with_secret_key( params[:account_id], params[:secret_key] )
+  return false unless @account
+
+  true
+end
+
+
+def require_token!
+  return false unless token_present?
+  
+  @token = Token.get( params[:token_id] )
+  return false unless @token
+
+  @account = Account.get( @token.account_id )
+  return false unless @account
+
+  true
+end
+
+
+def invalid_credentials!
+  status 401
+  @errors.add "Invalid credentials."
+end
+
+
 doc '/v1/tokens'
 get '/v1/tokens.json' do
-  t = Token.get( params[:token_id] )
-  if t
-    @out = t
-  else
-    status 404
-    @errors.add "Invalid token."
+
+  # single token check
+  if token_present?
+    if require_token!
+      @out = @token
+    else
+      invalid_credentials!
+    end
+    return
   end
+
+  # list of tokens for an account
+  if secret_auth_present?
+    if require_secret_key!
+      ts = Token.all_for( @account.account_id )
+      @out = { tokens: ts }
+    else
+      invalid_credentials!
+    end
+    return
+  end
+
+  invalid_credentials!
 end
 
 post '/v1/tokens.json' do
-  not_implemented
+  if require_secret_key!
+    token = Token.create!( @account.account_id )
+    @out = token
+    return
+  end
+  
+  invalid_credentials!
 end
 
 delete '/v1/tokens.json' do
