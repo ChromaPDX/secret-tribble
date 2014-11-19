1955469b45f159e2e4e563ed21e0cd81295462b3
Peat Bakke <peat@peat.org>
2014-11-19 09:33:00 -0800

    Axing verbose STDOUT output

--- src/bin/revenue-processor.rb
+++ src/bin/revenue-processor.rb
@@ -8,13 +8,15 @@ revenue_queue = PersistentQueue.new("revenue_transactions")
 distribution_queue = PersistentQueue.new("distribution_transactions")
 
 while true
+  start_time = Time.now
+
   while revenue_queue.size == 0
     puts "Sleeping ..."
     sleep POLLING_DELAY
   end
 
   revenue_queue.pop do |msg|
-    puts "Received message: #{msg.inspect}"
+    # puts "Received message: #{msg.inspect}"
     
     body = msg[:body]
     pool_id = body['pool_id']
@@ -22,10 +24,10 @@ while true
 
     # first, enter this as revenue in the ledger for the appropriate wallet
     origin_wallet = Wallet.with_kind_currency( Wallet::ORIGIN_KIND, body['currency'] )
-    puts "Origin wallet: #{origin_wallet.inspect}"
+    # puts "Origin wallet: #{origin_wallet.inspect}"
 
     revenue_wallet = Wallet.get( body['wallet_id'] )
-    puts "Revenue wallet: #{revenue_wallet.inspect}"
+    # puts "Revenue wallet: #{revenue_wallet.inspect}"
     
     ledger_entry = Ledger.new( to_wallet_id: revenue_wallet.wallet_id,
                                from_wallet_id: origin_wallet.wallet_id,
@@ -35,15 +37,15 @@ while true
                                origin: body['origin'].to_json,
                                transaction_id: body['transaction_id'] )
     ledger_entry.save!
-    puts "Ledger entry: #{ledger_entry.inspect}"
+    # puts "Ledger entry: #{ledger_entry.inspect}"
     
     # second, create the distribution messages
     pool = Pool.new( pool_id )
     pool.load!
-    puts "Pool: #{pool.inspect}"
+    # puts "Pool: #{pool.inspect}"
 
     distributions = pool.distribute( amount )
-    puts "Distributions: #{distributions.inspect}"
+    # puts "Distributions: #{distributions.inspect}"
 
     distributions.each do |user_id, amount|
       # use the original message as the base message
@@ -54,10 +56,11 @@ while true
       new_msg['user_id'] = user_id
       new_msg['amount'] = "%.4f" % amount
       new_msg.delete 'wallet_id'
-      puts "Distribution Message: #{new_msg.inspect}"
+      # puts "Distribution Message: #{new_msg.inspect}"
       distribution_queue.push( new_msg )
     end
-    
+
+    puts "Elapsed Time: #{Time.now - start_time}"
   end
 
 end
