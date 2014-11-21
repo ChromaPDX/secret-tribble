91ce98b1237ec0ce8e3d690cf563358f2dfe7607
Peat Bakke <peat@peat.org>
2014-11-20 16:39:43 -0800

    ermagerd, all of the hacking.

--- src/api/v1/static/src/app.js
+++ src/api/v1/static/src/app.js
@@ -64,6 +64,7 @@ app.controller("AuthenticationController", ['$scope', '$http', '$window', functi
 		// console.log("SUCCESS");
 		// console.log(data);
 		setToken(data.token_id);
+		location.reload(); // refresh the page with the token intact.
 	    })
 	    .error( function(data, status, headers, config) {
 		// console.log("ERROR");
@@ -79,12 +80,100 @@ app.controller("AuthenticationController", ['$scope', '$http', '$window', functi
 
 }]);
 
-app.controller("ProjectController", ['$scope', '$http', '$window', function($scope, $http, $window) {
+app.controller("WalletController", ['$scope', '$http', '$window', function($scope, $http, $window) {
 
     $scope.token = function() { return $window.sessionStorage.token_id; }
+    $scope.wallets = [];
+    $scope.wallet = false;
+    $scope.ledger = [];
+    $scope.wallet_revenue = 0.0;
 
+    $scope.updateRevenue = function() {
+	$scope.wallet_revenue = 0.0;
+	for ( i = 0; i < $scope.ledger.length; i++ ) {
+	    l = $scope.ledger[i];
+	    $scope.wallet_revenue += parseFloat( l.amount );
+	}
+    }
+    
+    $scope.loadWallet = function( wallet_id ) {
+	$scope.wallet = false;
+	$http({ method: 'GET',
+		url: '/v1/wallets.json',
+		params: { token_id: $scope.token(), wallet_id: wallet_id } })
+	    .success( function(data, status, headers, config) {
+		$scope.wallet = data;
+		$scope.loadLedger();
+	    })	
+    }
+
+    $scope.loadRelationWallet = function( relation_id ) {
+	$scope.wallet = false;
+	$http({ method: 'GET',
+		url: '/v1/wallets.json',
+		params: { token_id: $scope.token(), relation_id: relation_id } })
+	    .success( function(data, status, headers, config) {
+		$scope.wallet = data;
+	    })	
+    }
+    
+    $scope.loadWallets = function() {
+	$scope.wallets = [];
+	$scope.wallet = false;
+	
+	$http({ method: 'GET',
+		url: '/v1/wallets.json',
+		params: { token_id: $scope.token() } })
+	    .success( function(data, status, headers, config) {
+		$scope.wallets = data;
+	    });
+    }
+
+    $scope.loadLedger = function() {
+	$scope.ledger = [];
+	$http({ method: 'GET',
+		url: '/v1/ledger.json',
+		params: { token_id: $scope.token(), to_wallet_id: $scope.wallet.wallet_id } })
+	    .success( function(data, status, headers, config) {
+		$scope.ledger = data;
+		$scope.updateRevenue();
+	    })	
+    }
+
+
+    $scope.init = function() {
+	if ($scope.token() && !$scope.wallets.length) {
+	    $scope.wallet = false;
+	    $scope.loadWallets();
+	}
+
+	return true;
+    }
+
+    $scope.init();
+
+}]);
+
+app.controller("ProjectController", ['$scope', '$http', '$window', function($scope, $http, $window) {
+    
+    $scope.token = function() { return $window.sessionStorage.token_id; }
+    $scope.projects = [];
+    $scope.project = false;
+    $scope.project_wallet;
+    $scope.project_ledger;
+    $scope.project_revenue = 0.0;
+
+    $scope.updateRevenue = function() {
+	$scope.project_revenue = 0.0;
+	for ( i = 0; i < $scope.project_ledger.length; i++ ) {
+	    l = $scope.project_ledger[i];
+	    $scope.project_revenue += parseFloat( l.amount );
+	}
+    }
+    
     $scope.loadProject = function( project_id ) {
 	$scope.project = false;
+	$scope.project_wallet = false;
 	$scope.pool = false;
 	$scope.users = [];
 	
@@ -93,8 +182,12 @@ app.controller("ProjectController", ['$scope', '$http', '$window', function($sco
 		params: { token_id: $scope.token(), project_id: project_id } })
 	    .success( function(data, status, headers, config) {
 		$scope.project = data;
+		$scope.loadProjectWallet( data.project_id );
 		$scope.loadPool( data.pool_id );
+		return true;
 	    })
+
+	return false;
     }
 
     $scope.loadProjects = function() {
@@ -108,6 +201,32 @@ app.controller("ProjectController", ['$scope', '$http', '$window', function($sco
 	    });
     }
 
+    
+    $scope.loadProjectWallet = function() {
+	$scope.project_wallet = false;
+	$http({ method: 'GET',
+		url: '/v1/wallets.json',
+		params: { token_id: $scope.token(), relation_id: $scope.project.project_id } })
+	    .success( function(data, status, headers, config) {
+		$scope.project_wallet = data;
+		$scope.loadProjectLedger();
+	    })	
+    }
+
+    $scope.loadProjectLedger = function() {
+	$scope.project_ledger = [];
+	$http({ method: 'GET',
+		url: '/v1/ledger.json',
+		params: { token_id: $scope.token(), to_wallet_id: $scope.project_wallet.wallet_id } })
+	    .success( function(data, status, headers, config) {
+		$scope.project_ledger = data;
+		$scope.updateRevenue();
+	    })	
+    }
+
+    
+    
+    
     $scope.loadPool = function( pool_id ) {
 	$scope.pool = false;
 	$scope.users = [];
@@ -135,12 +254,13 @@ app.controller("ProjectController", ['$scope', '$http', '$window', function($sco
     }
 
     $scope.init = function() {
-	$scope.projects = false;
-	$scope.pool = false;
-	$scope.users = [];
-	$scope.projects = [];
-
-	$scope.loadProjects();
+	if ($scope.token() && !$scope.projects.length) {
+	    $scope.projects = false;
+	    $scope.pool = false;
+	    $scope.users = [];
+	    
+	    $scope.loadProjects();
+	}
     }
 
     $scope.init()
